618baf1631e26f688e93cca083e61d154efff57b
davidasare <gohan.david11@gmail.com>
2021-05-19 21:40:08 +0100

    feat: Created components for the navigation to link to

---
+++ src/components/pages/AboutUs.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AboutUs() {
+    return (
+        <div>
+            <h1 className='about-us'>About Us</h1>
+        </div>
+    )
+}
---
+++ src/components/pages/Menu.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Menu(props) {
+    return (
+        <div>
+            <h1 className='menu'>Menu</h1>
+        </div>
+    )
+}
