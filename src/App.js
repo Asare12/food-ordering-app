f83df8fbe48de284f741329fd959f01b193e77b4
davidasare <gohan.david11@gmail.com>
2021-05-27 01:05:30 +0100

    Applying the login route to App.js

--- src/App.js
+++ src/App.js
@@ -5,10 +5,9 @@ import Footer from "./components/Footer";
 import Menu from "./components/pages/Menu";
 import AboutUs from "./components/pages/AboutUs";
 import Home from "./components/pages/Home";
-import Login from "./components/pages/Login";
-import SignUp from "./components/pages/SignUp";
 import ContactUs from "./components/pages/ContactUs";
 import NotFound from "./components/pages/NotFound";
+import Form from "./components/Form";
 
 function App() {
   return (
@@ -16,8 +15,7 @@ function App() {
       <Navigationbar />
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route path="/login" component={Login} />
-        <Route path="/signUp" component={SignUp} />
+        <Route path="/login" component={Form} />
         <Route path="/about" component={AboutUs} />
         <Route path="/contact" component={ContactUs} />
         <Route path="/menu" component={Menu} />
