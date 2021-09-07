86d823c4635b8524f97e35f0aabc4e60f1775549
davidasare <gohan.david11@gmail.com>
2021-09-07 13:52:17 +0100

    Removing comment

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 
 import UserService from "../services/user.service";
-//import UserService from ".../"
 
 const Home = () => {
   const [content, setContent] = useState("");
